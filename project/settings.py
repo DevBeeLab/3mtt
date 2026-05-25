0ea1bb11ccc9da44847a2e8d782087fc9c3a7292
DevBee <walexsson@gmail.com>
2026-05-25 12:16:13 +0100

    add static

--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,7 @@ LOGIN_URL = '/authlogin'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
